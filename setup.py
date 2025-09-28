753f48789eede3a07371eb891b3668cb371611af
Evgenij Dzhivelikian <zhekauran@gmail.com>
2025-09-28 21:37:16 +0300

    fix url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='epm',
     version='1.1',
     description='Structuring episodic memory',
-    url='https://github.com/AIRI-Institute/him-agent',
+    url='https://github.com/Cognitive-AI-Systems/him-agent',
     author='Evgenii Dzhivelikian',
     author_email='zhekauran@gmail.com',
     license='AGPLv3',
